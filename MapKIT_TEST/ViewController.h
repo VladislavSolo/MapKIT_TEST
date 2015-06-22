4fc0df9f0ad33a1664d881889f6201bed2562eb5
Vladislav Stanisheskij <Vladislav-work1909@yandex.by>
2015-06-22 11:57:58 +0300

    Initial Commit

---
+++ MapKIT_TEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapKIT_TEST
+//
+//  Created by Владислав Станишевский on 6/22/15.
+//  Copyright (c) 2015 Vlad Stanishevskij. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
